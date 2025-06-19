9ab8d647cd2e983907ffe55786ada3cf5fc28c0a
Arjun Viswaa <arjunviswaa@gmail.com>
2025-06-19 11:52:21 +0530

    Arrays Initial

---
+++ Arrays/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] nums = {13, 46, 24, 52, 20, 46, 9};
+        int[] nums = {3, 1, 2, 4, 1, 5, 2, 6, 4};
+        int n = nums.length;
+    }
+}
