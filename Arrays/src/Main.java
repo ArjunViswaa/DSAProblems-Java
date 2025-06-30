c06aa0da02067de2b43d3ddb0315d4b2bf9a7ddf
Arjun Viswaa <arjun.viswanath@rentokil-initial.com>
2025-06-30 08:55:53 +0530

    25_06_2025 DSA Practice commits

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -5,6 +5,7 @@ import medium.FAQ_Medium;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,6 +14,7 @@ public class Main {
         int[] sortedNums = {1, 1, 2, 2, 2, 2, 3, 4, 5, 5, 5, 6, 6, 7};
         int[] binarynums = {1, 1, 0, 0, 1, 1, 1, 0, 0};
         int[] posNnegNums = {2, 4, 5, -1, -3, -4};
+        int[][] MxNMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int n = nums.length;
 
         int targetIndex = fundamentals.Linear_Search.linearSearch(nums, 5);
@@ -32,8 +34,16 @@ public class Main {
 //        int numberOfUniqueElements = LogicBuilding.removeDuplicatesFromSortedArray(sortedNums, sortedNums.length);
 
 //        ArrayList<Integer> leadersInArray = FAQ_Medium.leadersInArray(nums);
-        int[] rearrangeArrayElementsBySign = FAQ_Medium.rearrangeArrayElementsBySign(posNnegNums);
-        System.out.println(Arrays.toString(rearrangeArrayElementsBySign));
-//        System.out.println(leadersInArray);
+
+//        int[] rearrangeArrayElementsBySign = FAQ_Medium.rearrangeArrayElementsBySign(posNnegNums);
+
+        List<Integer> printMxNMatrixInSpiral = FAQ_Medium.printMxNMatrixInSpiral(MxNMatrix);
+
+        int pascalsTriangleRthCthElement = FAQ_Medium.pascalTriangleI(4, 2);
+//        int[] pascalsTriangleRthRow = FAQ_Medium.pascalTriangleII(5);
+        List<List<Integer>> pascalsTriangle = FAQ_Medium.pascalTriangleIII(5);
+
+//        System.out.println(Arrays.toString(pascalsTriangleRthRow));
+        System.out.println(pascalsTriangle);
     }
 }
--- Arrays/src/medium/FAQ_Medium.java
+++ Arrays/src/medium/FAQ_Medium.java
@@ -1,7 +1,9 @@
 package medium;
 
 import java.util.ArrayList;
+import java.util.*;
 import java.util.Collections;
+import java.util.List;
 
 public class FAQ_Medium {
     public static ArrayList<Integer> leadersInArray(int[] nums) {
@@ -33,6 +35,137 @@ public class FAQ_Medium {
             }
             idx++;
         }
+
+        return ans;
+    }
+
+    public static List<Integer> printMxNMatrixInSpiral(int[][] matrix) {
+        List<Integer> ans = new ArrayList<Integer>();
+        int left = 0, right = matrix[0].length - 1;
+        int top = 0, bottom = matrix.length - 1;
+
+        while(top <= bottom && left <= right) {
+            for(int i=left; i<=right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            for(int i=top; i<=bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+            if(top <= bottom) {
+                for(int i=right; i>=left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left <= right) {
+                for(int i=bottom; i>=top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int pascalTriangleI(int r, int c) {
+        long res = 1;
+        r--; c--;
+
+        int iter = Math.min(c, r - c);
+        for(int i=0; i<iter; i++) {
+            res = res * (r - i);
+            res = res / (i + 1);
+        }
+
+        return (int) res;
+    }
+
+    public static int[] pascalTriangleII(int r) {
+        int[] res = new int[r];
+        res[0] = 1;
+
+        for(int i=1; i<r; i++) {
+            res[i] = res[i - 1] * (r - i) / i;
+        }
+
+        return res;
+    }
+
+    public static  List<Integer> pascalTriangleIIList(int r) {
+        List<Integer> res = new ArrayList<Integer>();
+        res.add(1);
+
+        for(int i=1; i<r; i++) {
+            res.add(res.get(i - 1) * (r - i) / i);
+        }
+
+        return res;
+    }
+
+    public static List<List<Integer>> pascalTriangleIII(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=1; i<=n; i++) {
+            ans.add(pascalTriangleIIList(i));
+        }
+
+        return ans;
+    }
+
+    public static void rotateMatrixBy90(int[][] matrix) {
+        // Brute : Here is to create another nxn matrix traverse through the whole array and put the elements in the right place.
+        // Optimal : Transpose the matrix and then reverse every row.
+        int n = matrix.length;
+
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int start = -1, end = -1;
+        int[][] sortedArr = new int[nums.length][2];
+
+        for(int i=0; i<nums.length; i++) {
+            int[] temp = new int[2];
+            temp[0] = nums[i];
+            temp[1] = i;
+            sortedArr[i] = temp;
+        }
+
+        Arrays.sort(sortedArr, (int[] n1, int[] n2) -> Integer.compare(n1[0], n2[0]));
+
+        while(left < right) {
+            int valueLeft = sortedArr[left][0];
+            int valueRight = sortedArr[right][0];
+            if(valueLeft + valueRight == target) {
+                start = sortedArr[left][1];
+                end = sortedArr[right][1];
+                break;
+            } else if(valueLeft + valueRight > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        int[] ans = new int[2];
+        ans[0] = start; ans[1] = end;
 
         return ans;
     }
